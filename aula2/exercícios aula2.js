ed724255f11bf15695a56aa99e73ca76715287b0
Rener <rener.almeida@gmail.com>
2021-06-28 15:55:17 -0300

    Aula 2

---
+++ aula2/exercícios aula2.js
@@ -0,0 +1,85 @@
+//Exercício 1
+{
+console.log("Olá Mundo")
+}
+
+//Exercício 2
+{
+let nome = 'Whatever'
+let idade = 20
+let usuarioAtivo = true
+console.log("Nome: ")
+console.log(nome)
+console.log("Idade: ")
+console.log(idade)
+console.log("Usuário está ativo no sistema?")
+console.log(usuarioAtivo)
+}
+
+//Exercício 3
+{
+console.log(2+3)
+console.log(3-2)
+let a = 10
+let b = 20
+console.log(a+b)
+let c = a+b
+console.log(c)
+let d = `A primeira variável é ${a}, a segunda é ${b}, a junção de ambas é ${c}`
+console.log(d)
+}
+{
+let d = "1"+1
+console.log(d)
+}
+{
+let d = 'Olá'+23
+console.log(d)
+}
+{
+let shortconcat = '3+2, 3-2, 10, 20, 30, 11, Olá23'
+let output = `Basicamente ${shortconcat} representam o resumo das saídas dos exercícios anteriores.`
+console.log(output)
+}
+
+//Exercício Plano de Saúde
+{
+let nome = 'José da Silva'
+let idade = 27
+let peso = 83.5
+let altura = 1.70
+let plano = true
+let IMC = peso / (altura ** 2)
+let resposta =  `${nome} tem ${idade} anos e seu IMC é de ${IMC}`
+console.log(resposta)
+}
+{
+let nome = 'Carlos de Souza'
+let idade = 28
+let peso = 80.1
+let altura = 1.76
+let plano = true
+let IMC = peso / (altura ** 2)
+let resposta =  `${nome} tem ${idade} anos e seu IMC é de ${IMC}`
+console.log(resposta)
+}
+{
+let nome = 'Aline Ferreira'
+let idade = 33
+let peso = 63.7
+let altura = 1.53
+let plano = false
+let IMC = peso / (altura ** 2)
+let resposta =  `${nome} tem ${idade} anos e seu IMC é de ${IMC}`
+console.log(resposta)
+}
+{
+let nome = 'Ana Paula'
+let idade = 26
+let peso = 55
+let altura = 1.62
+let plano = true
+let IMC = peso / (altura ** 2)
+let resposta =  `${nome} tem ${idade} anos e seu IMC é de ${IMC}`
+console.log(resposta)
+}
